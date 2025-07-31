5762e0ed92ab964c395fe978d12968bce18bc772
peyman <peyman.alad@gmail.com>
2025-07-31 15:38:37 +0330

    Fix MinIO

--- src/HodHod.Core/Storage/MinioFileManager.cs
+++ src/HodHod.Core/Storage/MinioFileManager.cs
@@ -107,23 +107,7 @@ public class MinioFileManager : IMinioFileManager, ISingletonDependency
             preparedStream = buffer;
         }
 
-        //System.IO.Stream uploadStream = preparedStream;
-        var compressed = new MemoryStream();
-        using (var gzip = new GZipStream(compressed, CompressionLevel.Optimal, true))
-        {
-            preparedStream.Position = 0;
-            await preparedStream.CopyToAsync(gzip);
-        }
-        compressed.Position = 0;
-
-        System.IO.Stream uploadStream = compressed.Length < preparedStream.Length ? compressed : preparedStream;
-
-        //var headers = new Dictionary<string, string>();
-        //if (compressed.Length < preparedStream.Length)
-        //{
-        //    uploadStream = compressed;
-        //    headers["Content-Encoding"] = "gzip";
-        //}
+        System.IO.Stream uploadStream = preparedStream;
 
         var args = new PutObjectArgs()
             .WithBucket(_bucket)
@@ -132,31 +116,18 @@ public class MinioFileManager : IMinioFileManager, ISingletonDependency
             .WithStreamData(uploadStream)
             .WithObjectSize(uploadStream.Length);
 
-
-        //if (headers.Count > 0)
-        //{
-        //    args.WithHeaders(headers);
-        //}
-        if (uploadStream == compressed)
-        {
-            args.WithHeaders(new Dictionary<string, string>
-            {
-                ["Content-Encoding"] = "gzip"
-            });
-        }
         try
         {
             await _client.PutObjectAsync(args);
         }
-        finally
+        catch (Exception e)
         {
-            if (preparedStream != content)
-                await preparedStream.DisposeAsync();
-
-            if (uploadStream == compressed)
-                await compressed.DisposeAsync();
-            else
-                compressed.Dispose();
+            Console.WriteLine(e);
+            throw;
+        }
+        if (preparedStream != content)
+        {
+            await preparedStream.DisposeAsync();
         }
     }
 
@@ -206,31 +177,6 @@ public class MinioFileManager : IMinioFileManager, ISingletonDependency
 
         await _client.GetObjectAsync(args.WithCallbackStream(stream => stream.CopyTo(ms)));
         ms.Position = 0;
-
-        var isGzip = false;
-        if (stat?.MetaData != null)
-        {
-            var encKey = stat.MetaData.Keys
-                .FirstOrDefault(k => string.Equals(k, "Content-Encoding", StringComparison.OrdinalIgnoreCase));
-            if (encKey != null &&
-                string.Equals(stat.MetaData[encKey], "gzip", StringComparison.OrdinalIgnoreCase))
-            {
-                isGzip = true;
-            }
-        }
-
-        if (isGzip && !offset.HasValue && !length.HasValue)
-        {
-            var decompressed = new MemoryStream();
-            using (var gzip = new GZipStream(ms, CompressionMode.Decompress, true))
-            {
-                await gzip.CopyToAsync(decompressed);
-            }
-            decompressed.Position = 0;
-            ms.Dispose();
-            return decompressed;
-        }
-
 
         return ms;
     }
